9277ce2edc42bbd4d9f0d816782f5d09128f37f4
Saqib-Tanmoy <saqib.tanmoy@gmail.com>
2017-09-24 12:59:11 +0600

    DB Gateway added

---
+++ PeopleManagementStation/DbGateway/ConnectionGateway.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppUserManagementSystem.DbGateway
+{
+    public class ConnectionGateway
+    {
+        protected SqlConnection connection;
+        string connectionString = @"server=tcp:KyotoServer,49172; Integrated Security = SSPI; database =KeyRegistar_ForTest;Connect Timeout=30";
+
+    }
+}
---
+++ PeopleManagementStation/DbGateway/ConnectionString.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppUserManagementSystem.DbGateway
+{
+    public class ConnectionString
+    {
+        public string DBConn = @"Data Source=tcp:KyotoServer,49172;Initial Catalog=KeyRegistar_ForTest;User=sa;Password=SystemAdministrator;Persist Security Info=True;";
+
+    }
+}
